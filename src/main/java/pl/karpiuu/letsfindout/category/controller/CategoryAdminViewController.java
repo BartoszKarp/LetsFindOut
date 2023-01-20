fa5dc70e8a6dc7edf5af4dbab797faae3c1dd54b
BartoszKarp <bartek.karp93@gmail.com>
2023-01-20 22:01:07 +0100

    lets start

---
+++ src/main/java/pl/karpiuu/letsfindout/category/controller/CategoryAdminViewController.java
@@ -0,0 +1,42 @@
+package pl.karpiuu.letsfindout.category.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.karpiuu.letsfindout.category.model.Category;
+import pl.karpiuu.letsfindout.category.service.CategoryService;
+
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/admin/categories")
+public class CategoryAdminViewController {
+
+    private final CategoryService categoryService;
+
+    public CategoryAdminViewController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public String indexView(Model model){
+        model.addAttribute("categories", categoryService.getCategories());
+
+        return "admin/category/index";
+    }
+
+    @GetMapping("{id}")
+    public String editView(Model model, @PathVariable UUID id){
+        model.addAttribute("category", categoryService.getCategory(id));
+
+        return "admin/category/edit";
+    }
+
+    @PostMapping("{id}")
+    public String edit(@ModelAttribute("category") Category category, @PathVariable UUID id){
+        categoryService.updateCategory(id, category);
+
+        return "redirect:/admin/categories";
+    }
+
+}
---
+++ src/test/java/pl/karpiuu/letsfindout/LetsFindOutApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.karpiuu.letsfindout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsFindOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
