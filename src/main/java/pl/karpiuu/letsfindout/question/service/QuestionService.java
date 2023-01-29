2c50b2a3a6f452f235df719e3d68e321dc6bfb41
BartoszKarp <bartek.karp93@gmail.com>
2023-01-29 11:27:08 +0100

    Correct logic in QuestionService class

--- src/main/java/pl/karpiuu/letsfindout/question/service/QuestionService.java
+++ src/main/java/pl/karpiuu/letsfindout/question/service/QuestionService.java
@@ -1,5 +1,6 @@
 package pl.karpiuu.letsfindout.question.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -12,13 +13,10 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class QuestionService {
 
-	private QuestionRepository questionRepository;
-
-	public QuestionService(QuestionRepository questionRepository) {
-		this.questionRepository = questionRepository;
-	}
+	private final QuestionRepository questionRepository;
 
 	@Transactional(readOnly = true)
 	public List<Question> getQuestions() {
