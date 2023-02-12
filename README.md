61bd536198c4299fd50f8ce667b0a77a06e5426e
BartoszKarp <bartek.karp93@gmail.com>
2023-02-12 15:10:30 +0100

    .

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # LetsFindOut
+
+#### Do you want to know the answer about something? Great! You are in good place! LetsFindOut is application where You can find the answer on any question you want and any topic you like. Maybe you know the answer on question which was asked for someone else? Share it!
+
+## Table of Contents
+* [General Information](#general-information)
+* [Technologies Used](#technologies-used)
+* [Screenshots](#screenshots)
+* [Prerequisites](#prerequisites)
+* [Setup](#setup)
+* [Authors](#authors)
+
+## General Information
+
+LetsFindOut has been divided into two parts: Administrator and User.
+
+As a user, you can not only browse and search for answers. You can also give answers. In addition, you can check which questions have the most answers or which have not been answered.
+
+On the administrator's side, you can check the statistics of answers and questions asked. If you find that a question or category does not meet the requirements, you can: view, edit or delete.
+
+The project is aimed at practical learning, implementing the knowledge gained in Tomasz Woliński's courses - JOP (Java from scratch) and EAI (Efficient Internet Applications).
+The application is based on the REST architecture and the Minimum Viable Product (MVP) model.
+
+## Technologies Used
+### Development
+- [Java 19](https://openjdk.org/projects/jdk/19/)
+- [Spring Boot 2](https://spring.io/projects/spring-boot)
+- [Spring Data](https://spring.io/projects/spring-data)
+- [PostgreSQL (docker)](https://www.postgresql.org/)
+- [Maven 3.x](https://maven.apache.org/)
+- [Git](https://git-scm.com/)
+
+
+### Test
+- [JUnit5](https://junit.org/junit5/)
+- [Mockito](https://site.mockito.org/)
+
+## Screenshots
+![1-IndexView](readme-img/LetsFindOut-1-IndexView.png)
+![2-HotQuestion](readme-img/LetsFindOut-2-HotQuestion.png)
+![3-QuestionAnswer](readme-img/LetsFindOut-3-QuestionAnswer.png)
+![4-AdminHome](readme-img/LetsFindOut-4-AdminHome.png)
+![5-AdminDelete](readme-img/LetsFindOut-5-AdminDelete.png)
+![6-AdminEdit](readme-img/LetsFindOut-6-AdminEdit.png)
+
+## Prerequisites
+The following tools are required to start the application:
+
+- [IntelliJ IDEA](https://www.jetbrains.com/idea/),
+- [Java 19](https://openjdk.org/projects/jdk/19/),
+- [Maven 3.x](https://maven.apache.org/download.cgi),
+- [Docker](https://docs.docker.com/get-docker/)
+
+## Setup
+
+### To run this project 
+
+**1.** Clone repository:
+
+    git clone git@github.com:BartoszKarp/TinyUrl.git
+
+
+**2.** After download project configurate your container in Docker and db server in few step:
+
+- Create database connection with Docker pasting in command line:
+  
+      docker run --name postgrestinyurl -e POSTGRES_PASSWORD=password -d -p 5432:5432 postgres
+
+- Connect with server:
+
+      Login: postgres
+      Password: password
+
+
+## Authors
+Created by StormIT community:
+- bartek.karp93@gmail.com
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,17 +12,9 @@ insert into categories (id, name) values
                                       (gen_random_uuid(), 'Finanse'),
                                       (gen_random_uuid(), 'Kulinaria'),
                                       (gen_random_uuid(), 'Komputery'),
-                                      (gen_random_uuid(), 'Osobiste'),
+                                      (gen_random_uuid(), 'Smartphone'),
                                       (gen_random_uuid(), 'Motoryzacja'),
-                                      (gen_random_uuid(), 'Polityka'),
-                                      (gen_random_uuid(), 'Praca'),
-                                      (gen_random_uuid(), 'Prezenty'),
-                                      (gen_random_uuid(), 'Zakupy'),
-                                      (gen_random_uuid(), 'Elektronika'),
-                                      (gen_random_uuid(), 'Rozrywka'),
-                                      (gen_random_uuid(), 'Sex'),
-                                      (gen_random_uuid(), 'Związki'),
-                                      (gen_random_uuid(), 'Inne');
+                                      (gen_random_uuid(), 'Polityka');
 
 -- select * from questions;
 
@@ -49,3 +41,26 @@ insert into answers (id, name, question_id) values
                                                 (gen_random_uuid(), 'Bieszczady', (select id from questions where name = 'Gdzie najlepiej spędzić wakacje z Polsce')),
                                                 (gen_random_uuid(), 'Mazury', (select id from questions where name = 'Gdzie najlepiej spędzić wakacje z Polsce'));
 
+insert into questions (id, name, category_id) values
+                                                  (gen_random_uuid(), 'Jaki smarthpone wybrać w 2023 roku?', (select id from categories where name = 'Smartphone')),
+                                                  (gen_random_uuid(), 'Budżetowe smartphone? 1000zł, 1500zł, 2000zł', (select id from categories where name = 'Smartphone'));
+
+insert into answers (id, name, question_id) values
+                                                (gen_random_uuid(), 'Iphone 14', (select id from questions where name = 'Jaki smarthpone wybrać w 2023 roku?')),
+                                                (gen_random_uuid(), 'Samsung S23', (select id from questions where name = 'Jaki smarthpone wybrać w 2023 roku?'));
+
+insert into questions (id, name, category_id) values
+                                                  (gen_random_uuid(), 'W co zagrać w 2023 roku?', (select id from categories where name = 'Gry')),
+                                                  (gen_random_uuid(), 'Jaką gre kupić dla syna na urodziny?', (select id from categories where name = 'Gry'));
+
+insert into answers (id, name, question_id) values
+                                                (gen_random_uuid(), 'Wiedźmin 3: Dziki Gon', (select id from questions where name = 'W co zagrać w 2023 roku?')),
+                                                (gen_random_uuid(), 'Cyberpunk 2077', (select id from questions where name = 'W co zagrać w 2023 roku?')),
+                                                (gen_random_uuid(), 'Rocket League', (select id from questions where name = 'W co zagrać w 2023 roku?')),
+                                                (gen_random_uuid(), 'Hogwart Legacy', (select id from questions where name = 'W co zagrać w 2023 roku?'));
+
+
+insert into answers (id, name, question_id) values
+                                                (gen_random_uuid(), 'Minecraft', (select id from questions where name = 'Jaką gre kupić dla syna na urodziny?')),
+                                                (gen_random_uuid(), 'Call of Duty', (select id from questions where name = 'Jaką gre kupić dla syna na urodziny?')),
+                                                (gen_random_uuid(), 'Counter Strike: Global Offensive', (select id from questions where name = 'Jaką gre kupić dla syna na urodziny?'));
